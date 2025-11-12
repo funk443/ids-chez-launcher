655cb675317b5b8c0a52ae479b63710cb7b1ce54
CToID <funk443@icloud.com>
2025-11-12 08:21:39 +0800

    Add UNIX build script.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -xe
+
+LIB_AND_INCLUDE_PATH="chez"
+
+gcc -O2 -Wall -Wextra -oapp \
+    -I"$LIB_AND_INCLUDE_PATH" \
+    c/launcher.c \
+    -L"$LIB_AND_INCLUDE_PATH" \
+    -lkernel -llz4 -lz -lm
